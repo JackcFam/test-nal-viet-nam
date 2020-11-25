97114885e43eb3918d77d421dabfeb1e935a0e90
NhatPham <43815615+JackcFam@users.noreply.github.com>
2020-11-26 05:29:08 +0700

    up

--- src/pages/Home/BlogNew.js
+++ src/pages/Home/BlogNew.js
@@ -1,23 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
+import { Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
 function BlogNew(props) {
-    
-    return (
-      <div className="warp-car-top">
-        <div className="top1">
-          <img src="https://i.imgur.com/PlpyMar.jpg" alt={1} />
-          <p className="text">Our Buyers Check List</p>
-        </div>
-        <div className="top1">
-          <img src="https://i.imgur.com/0NXcQhv.jpg" alt={1} />
-          <p className="text">How To Spot Fraud</p>
-        </div>
-        <div className="top1">
-          <img src="https://i.imgur.com/VUvvMyv.jpg" alt={1} />
-          <p className="text">Best Used Cars 2018</p>
-        </div>
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  const [topnew, setTopnew] = useState([]);
+  useEffect(() => {
+    async function getTopnew() {
+      const request = await fetch("https://test-nal.herokuapp.com/new-post");
+      const topnew = await request.json();
+      setTopnew(topnew);
+    }
+    getTopnew();
+  }, []);
+  return (
+    <div className="warp-car-top">
+      <div className="top1">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          {topnew.map((item) => {
+            return (
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={item.image}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>{item.title}</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
       </div>
-    );
+      <div className="top1">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          {topnew.map((item) => {
+            return (
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={item.image}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>{item.title}</h3>
+
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+      </div>
+      <div className="top1">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          {topnew.map((item) => {
+            return (
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={item.image}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>{item.title}</h3>
+                 
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+      </div>
+    </div>
+  );
 }
 
 export default BlogNew;
